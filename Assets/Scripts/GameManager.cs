611c22b77191e106ac8b06c2a9b56249765bc671
urfbound <jacobfparsons@live.com>
2018-12-02 00:30:52 -0500

    starting on GameManager things. next things to add are counting time, growing plants, attracting dinosaurs, dinosaurs eating each other, and then any story/cutscene things. after that it's back to art

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     public MapManager mapMgr;
+    public float timescale;
+    public int minuteInterval;
+
+    private enum currentMode { PAUSE, PLAY };
+
+    private object timeLock;
+    private bool isCountingTime;
+    private int date;
+    private static int currentHour;
+    private static int currentMinute;
 
 	// Use this for initialization
 	void Awake () {
+        timeLock = new object();
         mapMgr = GetComponent<MapManager>();
         initGame();
 	}
@@ -19,6 +30,32 @@ public class GameManager : MonoBehaviour {
 
     private void initGame()
     {
+        //set up timer to count passing of days
 
+    }
+
+    private void endPlayerDay()
+    {
+
+    }
+
+    //private IEnumerator dayTimer()
+    //{
+    //
+    //}
+
+    private void setIsCountingTime(bool newValue) { lock (timeLock) { isCountingTime = newValue; } }
+    private bool getIsCountingTime() { lock (timeLock) { return isCountingTime; } }
+    private int[] getCurrentTime() { lock(timeLock) { return new int[] { currentHour, currentMinute }; } }
+    private void iterateTime()
+    {
+        lock (timeLock)
+        {
+            currentMinute += minuteInterval;
+            while (currentMinute >= 60) { currentMinute -= 60; currentHour += 1; }
+
+            //set any UI things
+
+        }
     }
 }
